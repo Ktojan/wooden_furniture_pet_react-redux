2859ff7972adc768298333c9f7b5e1a45877735a
Andrey <karpovich.andrey@gmail.com>
2024-09-10 11:25:05 +0300

    wiring Redux store to app

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,17 @@
+import store from '../redux/store'
 import SiteHeader from '../components/header'
 import './globals.scss'
+import { Provider } from 'react-redux'
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-    <SiteHeader/>
-    <Component {...pageProps} />
+    <Provider store={store}>
+      <SiteHeader/>
+      <Component {...pageProps} />
+    </Provider>
     </>)
 }
 
 export default MyApp
+//export default wrapper.withRedux(MyApp);
